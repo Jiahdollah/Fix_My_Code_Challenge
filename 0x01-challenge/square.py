470ea18923da641618bc3a2f68696c301ecb48b5
peter njihia <peternjihia2001@gmail.com>
2023-07-18 17:02:06 -0700

    My square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area_of_my_rectangle(self):
+        """Calculate the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter_of_my_square(self):
+        """Calculate the perimeter of the square."""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print("Area:", s.area_of_my_rectangle())
+    print("Perimeter:", s.perimeter_of_my_square())
